aba8dd8f4afe460dd36fc9c46e15fb3b5a669045
yezhel <yezhelmr@gmail.com>
2017-05-27 16:44:39 -0500

    Creacion de pagina principal del catalogo

---
+++ california/resources/views/catalogo/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Boutique California</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<header id="header" class="">
+		<h1>Boutique California</h1>
+	</header><!-- /header -->
+	
+</body>
+</html>
